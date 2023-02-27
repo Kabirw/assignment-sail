355ec4d7c32a7ff0c71dcdb9b620c4860493486c
Kabir Wadhwa <kabir.wadhwa@d2kindia.com>
2023-02-27 23:48:07 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+username = 'kabir@forkingpaths.tech'
+password = "surat2011k@K"
+host='smtp.gmail.com'
+port='587'
---
+++ user_config.py
@@ -0,0 +1,3 @@
+receiver_email = "wadhwawadhwa100@gmail.com"
+Subject = "This is a test email"
+body = "Hi, Here is an update from the past week. Below you will find a summary of PRs raised in the past week"
