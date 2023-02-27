57b349bcaad3620966068ee13bb7a3d0bf1a0c62
Kabir Wadhwa <kabir.wadhwa@d2kindia.com>
2023-02-27 23:54:11 +0530

    initial commit

--- assignmnet.py
+++ assignmnet.py
@@ -1,17 +1,13 @@
-import urllib3
-from urllib3 import request
-import certifi 
 import json
 import pandas as pd
 import requests
-
-#http = urllib3.PoolManager(
-#       cert_reqs='CERT_REQUIRED',
-#       ca_certs=certifi.where()) 
-       
-#url = https://api.github.com/repos/public-apis/public-apis/pulls?state=all
-#r = request.get('GET', url)
-#r.status       
+from datetime import datetime, timedelta
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import config 
+import user_config
 
 
 url = 'https://api.github.com/repos/public-apis/public-apis/pulls?state=all'
@@ -27,9 +23,61 @@ if response.status_code == 200:
     df = pd.DataFrame(data)
     
     # Filter the DataFrame to only include the columns we want
-    df = df[['id', 'html_url', 'state', 'title', ]]
+    df = df[['id', 'state', 'title', 'created_at','updated_at','closed_at','merged_at']]
 else:
     print('Error: Failed to retrieve data')
 
-# Print the first few rows of the DataFrame
-print(df.head())
+
+
+df['created_at'] = pd.to_datetime(df['created_at'], format='%Y-%m-%d').dt.date
+df['updated_at'] = pd.to_datetime(df['updated_at'], format='%Y-%m-%d').dt.date
+df['closed_at'] = pd.to_datetime(df['closed_at'], format='%Y-%m-%d').dt.date
+
+
+now = datetime.today().date()
+one_week_ago = now - timedelta(weeks=1)
+
+filtered_df = df[(df['created_at'] >= one_week_ago) & (df['created_at'] <= now)]
+
+#type = df.dtypes
+
+#print (type)
+
+#print(filtered_df)
+
+
+sender_email = "alerts@d2kindia.com"
+receiver_email = user_config.receiver_email
+password = config.password
+
+message = MIMEMultipart()
+message["From"] = config.username
+message["To"] = receiver_email
+message["Subject"] = user_config.Subject
+
+body = user_config.body
+message.attach(MIMEText(body, "plain"))
+
+table_html = filtered_df.to_html(index=False)
+
+#attachment = MIMEApplication(csv, _subtype='csv')
+#attachment.add_header('content-disposition', 'attachment', filename='data.csv')
+
+message.attach(MIMEText(table_html, 'html'))
+
+# create SMTP session
+session = smtplib.SMTP(config.host, config.port)
+session.starttls() # enable security
+session.login(sender_email, password) # login with credentials
+
+text = message.as_string()
+session.sendmail(sender_email, receiver_email, text)
+session.quit()
+
+
+print('Mail Sent')
+#This code sends a plain text email from the sender's email address to the receiver's email address using the Gmail SMTP server. You can modify the code to include attachments or use HTML content instead of plain text by modifying the MIMEText and MIMEMultipart objects.
+
+
+
+
